92c08c7282fb640c886f6e0c4fd834ec2c9cd505
heisarun <heisarun@gmx.com>
2023-09-24 06:16:05 +0530

    removing git problems.

--- installGems.bat
+++ installGems.bat
@@ -2,7 +2,7 @@
 echo "running nokogiri"
 call gem install nokogiri
 echo "running open-uri"
-call gem install open-uri	
+call gem install open-uri
 echo "running time"
 call gem install time
 echo "running awesome_print"
